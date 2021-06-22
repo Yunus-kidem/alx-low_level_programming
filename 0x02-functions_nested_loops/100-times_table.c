f1aad3a1c2a595439b8bb89c604d316f086f6b00
Yunus-kidem <74186011+Yunus-kidem@users.noreply.github.com>
2021-06-22 23:57:17 +0300

    task 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,42 @@
 #include "holberton.h"
+
 /**
- * print_times_table - prints the times table for n.
- * @n: The multiplication table requested.
- * Return: Nothing.
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	int i, j, res;
+	int num, mult, prod;
 
-	if (!(n > 15 || n < 0))
+	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; num++)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				res = (i * j);
-				if (j != 0)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
 					_putchar(' ');
-				}
-				if (res < 10 && j != 0)
-				{
+				if (prod <= 9)
 					_putchar(' ');
-					_putchar(' ');
-					_putchar((res % 10) + '0');
-				}
-				else if (res >= 10 && res < 100)
+
+				if (prod >= 100)
 				{
-					_putchar(' ');
-					_putchar((res / 10) + '0');
-					_putchar((res % 10) + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (res >= 100 && j != 0)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((res / 100) + '0');
-					_putchar((res / 10) % 10 + '0');
-					_putchar((res % 10) + '0');
+					_putchar((prod / 10) + '0');
 				}
-				else
-					_putchar((res % 10) + '0');
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
