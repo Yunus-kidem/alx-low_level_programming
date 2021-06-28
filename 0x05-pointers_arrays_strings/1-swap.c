0c0642ccf1310aae91cbfe6f4937b077f40122b5
root <yunuskt363@gmail.com>
2021-06-28 12:38:01 -0700

    task 1 :)

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 
 /**
+ * swap_int - main function.
  * @a: Pointer with the address value.
  * @b: Pointer with the address value.
  * Description: This function takes a pointer to an int as parameter
