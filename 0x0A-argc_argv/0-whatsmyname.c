21ea6449012afd27795a2b56d0ffe2f9bb1fc071
Yunus-kidem <74186011+Yunus-kidem@users.noreply.github.com>
2021-07-13 11:48:14 +0300

    task #

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,17 @@
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - Funtion
- *
- * @argc: size og argv
- * @argv: array of programs
- *
- * Return: zero
- */
+* main - prints its name follwoed by a newline
+* @argc: number of arguments
+* @argv: the vector that hold the arguments
+*
+* Description: prints the current directory and the name like ./name
+* Return: returns the name.
+*/
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	(void)(argc);
-
-	printf("%s\n", argv);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
