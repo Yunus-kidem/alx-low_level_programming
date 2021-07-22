ef08e1092b3c46d795fadfb9963ef478a1718e3b
Yunus-kidem <74186011+Yunus-kidem@users.noreply.github.com>
2021-07-22 13:50:54 +0300

    task #

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,85 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * print_c - Prints char
- * @list: arguments
- * Return: void
+ * print_int - prints int
+ * @list: arguments from print_all
  */
-void print_c(va_list list)
-{
-	printf("%c", (char)va_arg(list, int));
-}
-/**
- * print_d - Prints digit
- * @list: arguments
- * Return: void
- */
-void print_d(va_list list)
+void print_int(va_list list)
 {
 	printf("%d", va_arg(list, int));
 }
-/**
- * print_f - Prints float
- * @list: arguments
- * Return: void
- */
-void print_f(va_list list)
-{
-	printf("%f", (float)va_arg(list, double));
-}
-/**
- * print_str - Prints str
- * @list: arguments
- * Return: void
- */
-void print_s(va_list list)
-{
-	char *str = va_arg(list, char *);
 
-	if (str)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
+/**
+ * print_float - prints float
+ * @list: arguments from print_all
+ */
+void print_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
 }
 
 /**
- * print_all - Prints anything
- * @format: format to print
- * Return: void
+ * print_char - prints int
+ * @list: arguments from print_all
  */
+void print_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+}
+
+/**
+ * print_str - prints string
+ * @list: arguments from print_all
+ */
+void print_str(va_list list)
+{
+	char *s = va_arg(list, char *);
+
+	s == NULL ? printf("(nil)") : printf("%s", s);
+
+}
+
+/**
+ * print_all - prints any type
+ * @format: arguments to print
+ */
+
 void print_all(const char * const format, ...)
 {
-	va_list list;
-	char *separator = "";
-	int i = 0, j;
+va_list list;
+int i = 0, j = 0;
+char *sep = "";
 
-	filter filt[] = {
-	    {'c', print_c},
-	    {'i', print_d},
-	    {'f', print_f},
-	    {'s', print_s}
-	};
-	va_start(list, format);
-	while (format && format[i])
+printTypeStruct printType[] = {
+	{ "i", print_int },
+	{ "f", print_float },
+	{ "c", print_char },
+	{ "s", print_str },
+	{NULL, NULL}
+};
+
+
+va_start(list, format);
+
+while (format && format[i])
+{
+	j = 0;
+	while (j < 4)
 	{
-		j = 0;
-		while (j < 4)
+		if (*printType[j].type == format[i])
 		{
-			if (format[i] == filt[j].fmt)
-			{
-				printf("%s", separator);
-				filt[j].f(list);
-				separator = ", ";
-			}
-			j++;
+			printf("%s", sep);
+			printType[j].printer(list);
+			sep = ", ";
+			break;
 		}
-		i++;
+		j++;
 	}
-	printf("\n");
-	va_end(list);
+	i++;
 }
-/* hello Lau */
+
+printf("\n");
+va_end(list);
+}
